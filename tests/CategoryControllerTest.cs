159ff3b84b124c92418372ae207d79e2a6fb4155
Ruslan Gavshin <xgavsh00@vutbr.cz>
2021-03-12 01:07:50 +0100

    Udd test

---
+++ tests/CategoryControllerTest.cs
@@ -0,0 +1,182 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VUTProjectApp.Controllers;
+using VUTProjectApp.Data;
+using VUTProjectApp.Dto;
+using VUTProjectApp.Models;
+using Xunit;
+
+namespace VUTAppProjectTests
+{
+    public class CategoryControllerTest : IDisposable
+    {
+        Mock<ICategoryAPIRepo> mockRepo;
+        AppProfile realProfile;
+        MapperConfiguration configuration;
+        IMapper mapper;
+        List<Category> mockData;
+
+        public CategoryControllerTest()
+        {
+            mockRepo = new Mock<ICategoryAPIRepo>();
+            realProfile = new AppProfile();
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+            mockData = new List<Category>();
+        }
+
+        public void Dispose()
+        {
+            mockRepo = null;
+            mapper = null;
+            configuration = null;
+            realProfile = null;
+            mockData = null;
+        }
+
+        [Fact]
+        public async void GetGategories_ReturnListOfCategory_WhenDBIsNotEmpty()
+        {                    
+            mockRepo
+                .Setup(repo => repo.GetAllCategory())
+                .Returns(() => Task.FromResult(mockData));      
+
+            var categoryController = new CategoryController(mockRepo.Object, mapper);
+            var result = await categoryController.GetAllCategory();
+            Assert.IsType<ActionResult<List<CategoryDto>>>(result);
+        }        
+
+        [Fact]
+        public async void GetGategoryById_Returns404NotFound_WhenNonExistentIDProvider()
+        {            
+            mockRepo
+                .Setup(repo => repo.GetCategoryById(0))
+                .Returns(() => null);
+
+            var categoryController = new CategoryController(mockRepo.Object, mapper);
+
+            var result = await categoryController.GetCategoryById(1);            
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+
+        [Fact]
+        public async void GetGategoryById_Returns200Ok_WhenWhenValidIDProvided()
+        {
+            mockData.Add(new Category
+            {
+                Id = 1,
+                Name = "Test"
+            });
+            mockRepo
+                .Setup(repo => repo.GetCategoryById(1)) 
+                .Returns(() => Task.FromResult(mockData[0]));
+
+            var categoryController = new CategoryController(mockRepo.Object, mapper);
+            var result = await categoryController.GetCategoryById(1);
+
+            Assert.IsType<ActionResult<CategoryDto>>(result);            
+        }
+
+        [Fact]
+        public void CreateCategory_ReturnsCorrectResourceType_WhenValidObjectSubmitted()
+        {
+            mockData.Add(new Category
+            {
+                Id = 0,
+                Name = "test"
+            });
+            mockRepo.Setup(repo => repo.GetCategoryById(1))
+                .Returns(Task.FromResult(mockData[0]));
+            var controller = new CategoryController(mockRepo.Object, mapper);
+
+            var result = controller.CreateCategory(new CategoryCreateDto());
+
+            Assert.IsType<ActionResult<CategoryDto>>(result);
+
+        }
+
+        [Fact]
+        public void CreateCategory_Returns201Created_WhenValidObjectSubmitted()
+        {
+            mockData.Add(new Category
+            {
+                Id = 0,
+                Name = "test"
+            });
+            mockRepo.Setup(repo => repo.GetCategoryById(1))
+                .Returns(Task.FromResult(mockData[0]));
+            var controller = new CategoryController(mockRepo.Object, mapper);
+
+            var result = controller.CreateCategory(new CategoryCreateDto());
+
+            Assert.IsType<CreatedAtRouteResult>(result.Result);
+        }
+
+        [Fact]
+        public void UpdateCategory_Returns204NoContent_WhenValidObjectSubmitted()
+        {
+            mockData.Add(new Category
+            {
+                Id = 0,
+                Name = "Test"
+            });
+            mockRepo
+                .Setup(repo => repo.GetCategoryById(1))
+                .Returns(() => Task.FromResult(mockData[0]));
+            var controller = new CategoryController(mockRepo.Object, mapper);
+
+            var result = controller.UpdateCategory(1, new CategoryCreateDto());
+
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public void UpdateCategory_Returns404NotFound_WhenNonExistentResourceIDSubmitted()
+        {
+            mockRepo
+                .Setup(repo => repo.GetCategoryById(1))
+                .Returns(() => null);
+            var controller = new CategoryController(mockRepo.Object, mapper);
+
+            var result = controller.UpdateCategory(1, new CategoryCreateDto());
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void DeleteCommand_Returns204NoContent_WhenValidResourceIDSubmitted()
+        {
+            mockData.Add(new Category
+            {
+                Id = 0,
+                Name = "Test"
+            });
+            mockRepo
+                .Setup(repo => repo.GetCategoryById(1))
+                .Returns(() => Task.FromResult(mockData[0]));
+            var controller = new CategoryController(mockRepo.Object, mapper);
+            
+            var result = controller.DeleteCategory(1);
+            
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public void DeleteCommand_Returns404NotFound_WhenNonExistentResourceIDSubmitted()
+        {            
+            mockRepo
+                .Setup(repo => repo.GetCategoryById(0))
+                .Returns(() => null);
+            var controller = new CategoryController(mockRepo.Object, mapper);
+
+            var result = controller.DeleteCategory(0);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+    }
+}
